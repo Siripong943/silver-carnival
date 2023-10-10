75dd1afb8477586543c67e44b7cccee445c1aef9
Siripong943 <140494055+Siripong943@users.noreply.github.com>
2023-10-10 01:08:41 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
-import './App.css';
+import React, { useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+const BmiCalculator = () => {
+    const w_inputRef = useRef(null);
+    const h_inputRef = useRef(null);
+    const [bmi, setBmi] = useState(null);
 
-export default App;
+    const calBmi = () => {
+        let w = parseFloat(w_inputRef.current.value);
+        let h = parseFloat(h_inputRef.current.value);
+
+        if (isNaN(w) || isNaN(h)) {
+            alert("Please enter valid weight and height.");
+            return;
+        }
+
+        const calculatedBmi = w / Math.pow(h / 100, 2);
+        setBmi(calculatedBmi);
+    };
+
+    const resetInputs = () => {
+        w_inputRef.current.value = '';
+        h_inputRef.current.value = '';
+        setBmi(null);
+    };
+
+    const getBmiCategory = (bmi) => {
+        if (bmi < 18.5) return 'ผอม';
+        if (bmi >= 18.5 && bmi < 25) return 'ปกติ';
+        if (bmi >= 25 && bmi < 30) return 'ท้วม';
+        return 'อ้วน';
+    };
+
+    return (
+        <>
+            <h3>BMI Calculator</h3>
+            Weight : <input
+                type="text"
+                ref={w_inputRef}
+                placeholder="Enter weight in kg."
+            /> kg.<br />
+            Height : <input
+                type="text"
+                ref={h_inputRef}
+                placeholder="Enter height in cm."
+            /> cm.<br />
+            <button onClick={calBmi}>Calculate</button>
+            <button onClick={resetInputs}>Reset</button><br />
+            {bmi !== null && (
+                <>
+                    BMI value: {bmi.toFixed(2)} ({getBmiCategory(bmi)})
+                    <BmiText bmiCategory={getBmiCategory(bmi)} />
+                </>
+            )}
+        </>
+    );
+};
+
+const BmiText = ({ bmiCategory }) => {
+    return <h1>{bmiCategory}</h1>;
+};
+
+export default BmiCalculator;
